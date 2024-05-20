bd0643b6d0f08bebab2af7e267ad318a3cdc37c8
Patrick <patrickbell1302@gmail.com>
2024-05-20 19:34:34 +0100

    update

--- public/project.js
+++ public/project.js
@@ -1,2 +1,92 @@
-const urlParams = new URLSearchParams(window.location.search)
-const projectId = urlParams.get('id')
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name');
+    const picture = urlParams.get('image');
+    const projectId = urlParams.get('id');
+    const techStack = urlParams.get('techStack').split(',');
+    const description = urlParams.get('description');
+    const features = urlParams.get('features').split(',');
+    const improvements = urlParams.get('improvements').split(',');
+    const sourceCode = urlParams.get('sourceCode');
+    const productionWebsite = urlParams.get('productionWebsite');
+
+    const encodedSampleImages = urlParams.get('sampleImages');
+    const decodedSampleImages = decodeURIComponent(encodedSampleImages);
+    const sampleImages = JSON.parse(decodedSampleImages);
+
+    const projectWebsite = document.getElementById('project-website');
+    projectWebsite.innerHTML = `<a href="${productionWebsite}"><i class="fa-regular fa-hand-point-right"></i> Go to Website</a>`;
+
+    const projectCode = document.getElementById('project-code');
+    projectCode.innerHTML = `<a href="${sourceCode}"><i class="fa-regular fa-hand-point-right"></i> Go to Github Code</a>`;
+
+    const projectTitle = document.getElementById('project-title');
+    projectTitle.innerHTML = `Project ${projectId} - ${name}`;
+
+    const projectDesc = document.getElementById('project-description');
+    projectDesc.innerHTML = description;
+
+    const projectFeatures = document.getElementById('project-features');
+    const formattedFeatures = features.map(feature => `<li>${feature}</li>`);
+    projectFeatures.innerHTML = formattedFeatures.join('');
+
+    const projectTechStack = document.getElementById('project-techstack');
+    const formattedStack = techStack.map(tech => `<li>${tech}</li>`);
+    projectTechStack.innerHTML = formattedStack.join('');
+
+    const projectImprovements = document.getElementById('project-improvements');
+    const formattedImprovements = improvements.map(improve => `<li>${improve}</li>`);
+    projectImprovements.innerHTML = formattedImprovements.join('');
+
+    const length = sampleImages.length;
+    console.log(length);
+
+    const swiperWrapper = document.querySelector('.swiper-wrapper');
+    let loadedImagesCount = 0;
+
+    function checkAllImagesLoaded() {
+        loadedImagesCount++;
+        if (loadedImagesCount === sampleImages.length) {
+            initializeSwiper();
+        }
+    }
+
+    sampleImages.forEach((img, index) => {
+        const slide = document.createElement('div');
+        slide.className = 'swiper-slide';
+
+        const slideText = document.createElement('div');
+        slideText.className = 'slide-text'; // Adding a class for styling
+        slideText.innerHTML = `Showing image <strong>${index + 1}</strong> out of <strong>${length}</strong> - ${img.text}`;
+
+        const image = document.createElement('img');
+        image.className = 'swiper-img';
+        image.src = img.image;
+        image.onload = checkAllImagesLoaded;
+        image.onerror = checkAllImagesLoaded;
+
+        slide.appendChild(slideText); // Append the text before the image
+        slide.appendChild(image);
+
+        swiperWrapper.appendChild(slide);
+    });
+
+    function initializeSwiper() {
+        const swiper = new Swiper('.swiper', {
+            direction: 'horizontal',
+            loop: true,
+            autoplay: {
+                delay: 2500,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        });
+    }
+});
--- public/script.js
+++ public/script.js
@@ -13,13 +13,13 @@ let projects = [
         'Admin login with sample account using passport.js',
         'Filtering and search system for product page', 
         'Contact us section using Nodemailer', 
-        'Automatic emails for purchases and status updates (e.g. pending, shipped, delivered)', 
+        'Automatic emails for purchases and status updates (e.g pending/shipped/delivered)', 
         'Validation for contact us form',
         'Shipping options when checking out with Stripe', 
         'Promotion Codes', 
         'Chatbot (currently not working)', 
         'Order page on admin dashboard that shows all orders, sorted in chronological order with filtering',
-        'Toasts for adding, removing and out of stock products', 
+        'Toasts for adding / removing and out of stock products', 
         'Light and dark mode options on the admin dashboard',
         'Inventory Management & out of stock products',
         'Scheduled emails for low stock items using node-cron'
@@ -30,6 +30,19 @@ let projects = [
         sourceCode: 'https://github.com/Patrick-Bell/test',
         productionWebsite: 'https://test-admin-wdmf.onrender.com',
         warning: 'As websites are hosted on render, they may take up to 60 seconds to load.',
+        sampleImages: [
+            {image: '/images/cointology.png', text: 'Cointology Logo'},
+            {image: '/images/cointology-1.png', text: 'Cointology Home Page'},
+            {image: '/images/cointology-2.png', text: 'Cointology Product Page'},
+            {image: '/images/cointology-3.png', text: 'Cointology Cart Page'},
+            {image: '/images/cointology-4.png', text: 'Cointology Contact Us Page'},
+            {image: '/images/cointology-5.png', text: 'Cointology Admin Login Page'},
+            {image: '/images/cointology-6.png', text: 'Cointology Admin Product Page (lightmode)'},
+            {image: '/images/cointology-7.png', text: 'Cointology Admin Product Page (darkmode)'},
+            {image: '/images/cointology-8.png', text: 'Cointology Stripe Checkout Page'},
+            {image: '/images/cointology-9.png', text: 'Cointology Admin Order Page'},
+            {image: '/images/cointology-10.png', text: 'Cointology Add Product Modal'},
+        ],
     },
     {
         id: 2,
@@ -37,7 +50,7 @@ let projects = [
         image: '/images/timeticker-mockup.png',
         techStack: ['HTML', 'CSS', 'JavaScript', 'NodeJS', 'ExpressJS', 'MongoDB'],
         description: 'EventTicker is a comprehensive full-stack application designed to empower users in managing and tracking various events seamlessly. Offering a user-friendly interface, it facilitates the creation, deletion, and countdown of events, enabling individuals to stay organized and informed about upcoming occasions. With a focus on user engagement, EventTicker incorporates robust user account functionality, allowing users to register, log in, and personalize their experience. Moreover, users can share their insights and experiences by writing reviews for events, fostering a vibrant community atmosphere. To enhance user engagement and ensure timely event notifications, EventTicker provides automatic email reminders, ensuring that users never miss important milestones. Whether planning personal celebrations, professional engagements, or community gatherings, EventTicker simplifies event management and amplifies user connectivity.',
-        features: ['Add/delete events', 
+        features: ['Add / delete events', 
         'Ability to customise the colour of the event',
         'Days left displayed and updated daily',
         'User register and log in authentication using passport.js', 
@@ -49,6 +62,14 @@ let projects = [
         improvements: ['test'],
         sourceCode: 'https://test-admin-wdmf.onrender.com',
         productionWebsite: 'https://test-admin-wdmf.onrender.com',
+        sampleImages: [
+            {image: '/images/timeticker-1.png', text: 'TimeTicker Home Page'},
+            {image: '/images/timeticker-2.png', text: 'TimeTicker Review Page'},
+            {image: '/images/timeticker-3.png', text: 'TimeTicker Login Page'},
+            {image: '/images/timeticker-4.png', text: 'TimeTicker Register Page'},
+            {image: '/images/timeticker-5.png', text: 'TimeTicker Event Page'},
+            {image: '/images/timeticker-6.png', text: 'TimeTicker Profile Page (Write a Review with Validation)'},
+        ],
         warning: 'As websites are hosted on render, they may take up to 60 seconds to load.',
     },
     {
@@ -66,17 +87,31 @@ let projects = [
         'Job page with filtering system',
         'File upload managed with multer',
         'Admin login using passport.js',
-        'Extensive dashboard that shows all jobs, referrals, messages all with pagination',
-        'Admins can add, edit and delete jobs',
+        'Advanced dashboard that shows jobs / referrals /  messages - all with pagination',
+        'Admins can add / edit and delete jobs',
         'Ability to generate job reports that can be downloaded and/or printed',
         'Admins can send emails to all unique email subsribers using quill.js',
-        'Message page that stores all user messages and can be filtered by name, email, id, month, year and more',
-        'Animations throughout (including loading, buttons and more)',
+        'Message page that stores all user messages and can be filtered by name / email / id / month / year and more',
+        'Animations throughout (including loading / buttons and more)',
         'FAQ section'
         ],
         improvements: ['test'],
         sourceCode: 'https://test-admin-wdmf.onrender.com',
         productionWebsite: 'https://test-admin-wdmf.onrender.com',
+        sampleImages: [
+            {image: '/images/techtroop-1.png', text: 'TechTroop Home Page'},
+            {image: '/images/techtroop-2.png', text: 'TechTroop Calendy Integration'},
+            {image: '/images/techtroop-3.png', text: 'TechTroop Job Page with Filters'},
+            {image: '/images/techtroop-4.png', text: 'TechTroop Admin Login Modal'},
+            {image: '/images/techtroop-5.png', text: 'TechTroop Admin Page'},
+            {image: '/images/techtroop-6.png', text: 'TechTroop Job Details Page'},
+            {image: '/images/techtroop-7.png', text: 'TechTroop Job Application Form'},
+            {image: '/images/techtroop-8.png', text: 'TechTroop Job Confirmation Page'},
+            {image: '/images/techtroop-9.png', text: 'TechTroop Generate Report Page (print / download report)'},
+            {image: '/images/techtroop-10.png', text: 'TechTroop Messages Page (with filtering)'},
+            {image: '/images/techtroop-11.png', text: 'TechTroop Edit Job Modal'},
+            {image: '/images/techtroop-12.png', text: 'TechTroop View Job Applicants (can view CV, Linkedin Profile & more)'},
+        ],
         warning: 'As websites are hosted on render, they may take up to 60 seconds to load.',
     },
     {
@@ -85,18 +120,30 @@ let projects = [
         image: '/images/wordle-mockup.png',
         techStack: ['HTML', 'CSS', 'JavaScript'],
         description: 'Wordle is a clone from the original, with additional features such as stat tracking and achievements. This was my first project and was undertaken to grasp a solid understanding on basic coding principles such as data structures, data manipulation and algorithms',
-        features: ['Animations when words are guessed correctly/incorrectly', 
+        features: ['Animations when words are guessed correctly / incorrectly', 
         'Generates a random word per game', 
-        'Tracks user stats (games played, games won, longest win streak etc)', 
+        'Tracks user stats (games played / games won / longest win streak etc)', 
         'Ability to earn achievements', 
         'Option to learn more about the chosen word', 
-        'Share option to share user stats with friends'
-        ,'Displays time taken to guess word', 
+        'Share option to share user stats with friends',
+        'Displays time taken to guess word', 
         'Light/Dark mode', 
         'Web/Mobile Reponsive'],
         improvements: ['Include backend server to create user profiles, track badges and stats for each user to create leaderboards and online competitions'],
         sourceCode: 'https://test-admin-wdmf.onrender.com',
         productionWebsite: 'https://test-admin-wdmf.onrender.com',
+        sampleImages: [
+            {image: '/images/cointology-1.png', text: 'Checkout page'},
+            {image: '/images/cointology-2.png', text: 'Checkout page'},
+            {image: '/images/cointology-3.png', text: 'Checkout page'},
+            {image: '/images/cointology-4.png', text: 'Checkout page'},
+            {image: '/images/cointology-5.png', text: 'Checkout page'},
+            {image: '/images/cointology-6.png', text: 'Checkout page'},
+            {image: '/images/cointology-7.png', text: 'Checkout page'},
+            {image: '/images/cointology-8.png', text: 'Checkout page'},
+            {image: '/images/cointology-9.png', text: 'Checkout page'},
+            {image: '/images/cointology-10.png', text: 'Checkout page'},
+        ],
         warning: 'As websites are hosted on render, they may take up to 60 seconds to load.',
     },
 ]
@@ -132,19 +179,22 @@ const renderProjectsOnPage = () => {
 
 
     const viewBtns = document.querySelectorAll('.view');
-    viewBtns.forEach(btn => {
-        btn.addEventListener("click", (event) => {
-            const project = JSON.parse(event.target.dataset.project); // Use btn instead of event.target
-            const queryParams = new URLSearchParams();
-            for (const key in project) {
+viewBtns.forEach(btn => {
+    btn.addEventListener("click", (event) => {
+        const project = JSON.parse(event.target.dataset.project); // Use btn instead of event.target
+        const encodedSampleImages = encodeURIComponent(JSON.stringify(project.sampleImages));
+        const queryParams = new URLSearchParams();
+        for (const key in project) {
+            if (key !== 'sampleImages') { // Exclude sampleImages from normal parameters
                 queryParams.append(key, project[key]);
             }
-            queryParams.append('id', project.id);
-            const queryString = queryParams.toString();
-            window.location.href = `/projects?${queryString}`
-            console.log(url)
-        });
+        }
+        queryParams.append('sampleImages', encodedSampleImages); // Append encoded sampleImages
+        const queryString = queryParams.toString();
+        window.location.href = `/projects?${queryString}`;
     });
+});
+
     
 
 
